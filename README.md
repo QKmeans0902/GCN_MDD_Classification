af3aa7159ef9ab7a98f197627ca09921e15b30be
QKmeans0902 <58902380+QKmeans0902@users.noreply.github.com>
2023-04-14 21:15:54 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# GCN_MDD_Classification
+
+This repository provides core codes for the GCN model used in paper entitled "Using graph convolutional network to characterize individuals with major depressive disorder across multiple imaging sites".
---
+++ Utils.py
@@ -0,0 +1,38 @@
+import numpy as np
+from scipy.sparse import coo_matrix
+
+
+def compute_KNN_graph(matrix, k_degree=10):
+    """ Calculate the adjacency matrix from the connectivity matrix."""
+
+    matrix = np.abs(matrix)
+    idx = np.argsort(-matrix)[:, 0:k_degree]
+    matrix.sort()
+    matrix = matrix[:, ::-1]
+    matrix = matrix[:, 0:k_degree]
+
+    A = adjacency(matrix, idx).astype(np.float32)
+
+    return A
+
+
+def adjacency(dist, idx):
+
+    m, k = dist.shape
+    assert m, k == idx.shape
+    assert dist.min() >= 0
+
+    # Weight matrix.
+    I = np.arange(0, m).repeat(k)
+    J = idx.reshape(m * k)
+    V = dist.reshape(m * k)
+    W = coo_matrix((V, (I, J)), shape=(m, m))
+
+    # No self-connections.
+    W.setdiag(0)
+
+    # Non-directed graph.
+    bigger = W.T > W
+    W = W - W.multiply(bigger) + W.T.multiply(bigger)
+
+    return W.todense()
